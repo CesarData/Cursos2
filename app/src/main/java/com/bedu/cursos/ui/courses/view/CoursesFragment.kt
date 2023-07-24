d4b624ddfac473bd6646f46d5e9fb5e56f1ec30b
CesarData <cesaro.patafisico@gmail.com>
2023-07-24 14:15:52 -0600

    Carga inicial del proyecto.

---
+++ app/src/main/java/com/bedu/cursos/ui/courses/view/CoursesFragment.kt
@@ -0,0 +1,41 @@
+package com.bedu.cursos.ui.courses.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.navArgs
+import com.bedu.cursos.R
+//import com.bedu.cursos.FlowStepFragmentArgs
+import com.bedu.cursos.ui.courses.view.CoursesFragmentArgs
+
+class CoursesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        setHasOptionsMenu(true)
+
+        //val flowStepNumber = arguments?.getInt("flowStepNumber")
+
+        val safeArgs: CoursesFragmentArgs by navArgs()
+        //val flowStepNumber = safeArgs.flowStepNumber
+
+        return when (safeArgs.flowStepNumber) {
+            2 -> inflater.inflate(R.layout.flow_step_two_fragment, container, false)
+            else -> inflater.inflate(R.layout.flow_step_one_fragment, container, false)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<View>(R.id.next_button).setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.next_action)
+        )
+    }
+}
---
+++ app/src/main/java/com/bedu/cursos/ui/main/view/HomeFragment.kt
@@ -0,0 +1,47 @@
+package com.bedu.cursos.ui.main.view
+
+import android.os.Bundle
+import android.view.*
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
+//import com.bedu.cursos.ui.main.view.HomeFragmentDirections
+import com.bedu.cursos.R
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        setHasOptionsMenu(true)
+        return inflater.inflate(R.layout.home_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val options = navOptions {
+            anim {
+                enter = R.anim.slide_in_right
+                exit = R.anim.slide_out_left
+                popEnter = R.anim.slide_in_left
+                popExit = R.anim.slide_out_right
+            }
+        }
+
+        val btnDestination = view.findViewById<Button>(R.id.navigate_destination_button)
+        btnDestination?.setOnClickListener {
+            findNavController().navigate(R.id.flow_step_one_dest, null, options)
+        }
+        val btnAction = view.findViewById<Button>(R.id.navigate_action_button)
+        btnAction?.setOnClickListener {
+            //findNavController().navigate(R.id.next_action, null, options)
+            val flowStepNumberArg = 1
+            val action = HomeFragmentDirections.nextAction(flowStepNumberArg)
+            findNavController().navigate(action)
+        }
+    }
+}
